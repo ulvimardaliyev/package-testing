141906840968e61d79f114beca7cc59f32561804
Ulvi Mardaliyev <ulvi.mardaliyev@pashabank.az>
2023-01-03 17:40:35 +0400

    first packasge creating

---
+++ src/main/java/com/demoproject/gitdependency/DemoProjectGitDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.gitdependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectGitDependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectGitDependencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demoproject/gitdependency/DemoProjectGitDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.gitdependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectGitDependencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
